e9f202af492ca0a368e06607de95553e4a6d1620
Ivan Reshetnikov <ordinarydev@protonmail.com>
2022-08-16 23:05:00 +0500

    Update neovim config
    1. Add new plugins
    2. Make config look better
    3. Add some new options

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,19 +1,33 @@
-vim.opt.showmatch = true      -- show matching 
-vim.opt.ignorecase = true     -- case insensitive 
-vim.opt.hlsearch = true       -- highlight search 
-vim.opt.incsearch = true      -- incremental search
-vim.opt.tabstop = 4           -- number of columns occupied by a tab 
-vim.opt.softtabstop = 4       -- see multiple spaces as tabstops so <BS> does the right thing
-vim.opt.expandtab = true      -- converts tabs to white space
-vim.opt.shiftwidth = 4        -- width for autoindents
-vim.opt.autoindent = true     -- indent a new line the same amount as the line just typed
-vim.opt.number = true         -- add line numbers
-vim.opt.syntax = "enable"     -- syntax highlighting
-vim.opt.cursorline = true     -- highlight current cursorline
+local options = {
+    clipboard = "unnamedplus",   -- use system clipboard
+    wrap = false,                -- display lines as one long line
+    showmode = false,            -- hide mode (-- INSERT --)
+    showmatch = true,            -- show matching 
+    ignorecase = true,           -- case insensitive 
+    hlsearch = true,             -- highlight search 
+    incsearch = true,            -- incremental search
+    tabstop = 4,                 -- number of columns occupied by a tab 
+    softtabstop = 4,             -- see multiple spaces as tabstops so <BS> does the right thing
+    expandtab = true,            -- converts tabs to white space
+    shiftwidth = 4,              -- width for autoindents
+    autoindent = true,           -- indent a new line the same amount as the line just typed
+    number = true,               -- add line numbers
+    syntax = "enable",           -- syntax highlighting
+    cursorline = true,           -- highlight current cursorline
+}
+
+for key, value in pairs(options) do
+  vim.opt[key] = value
+end
 
 local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
 Plug 'ryanoasis/vim-devicons'
 Plug 'scrooloose/nerdtree'
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim'
+Plug 'nvim-lualine/lualine.nvim'
 vim.call('plug#end')
+
+require 'lualine-config'
